03bba955f44448a3921329821542429ae82372bb
tatsu0713 <kwbttk@icloud.com>
2020-09-22 20:39:55 +0900

    second commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :kanji_family_name, :kanji_first_name, :kana_family_name, :kana_first_name, :birthday])
+  # end
 end
