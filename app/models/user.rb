81b9d1565c7c8305019ab4b6b0efad3a00c884c1
tatsu0713 <kwbttk@icloud.com>
2020-09-28 17:54:31 +0900

    ユーザー管理のテストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,6 @@ class User < ApplicationRecord
   belongs_to_active_hash :favorite_car
 
   validates :nickname, presence: true
+  validates :password, length: { minimum: 8 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
   validates :favorite_car_id, numericality: { other_than: 1, message: 'を選択してください'}
 end
