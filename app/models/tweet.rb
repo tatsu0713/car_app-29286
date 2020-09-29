ed8d95a642540ce759195672b7547bbd97770591
tatsu0713 <kwbttk@icloud.com>
2020-09-29 20:16:54 +0900

    tweetsテーブルの作成

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,7 @@
 class Tweet < ApplicationRecord
+  belongs_to :user
+  has_many :comments
+  belongs_to_active_hash :favorite_car
+  belongs_to_active_hash :type
+  belomgs_to_active_hash :model_year
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_22_074839) do
+ActiveRecord::Schema.define(version: 2020_09_29_110617) do
+
+  create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.integer "brand_id", null: false
+    t.integer "type_id", null: false
+    t.integer "model_year_id", null: false
+    t.string "title", null: false
+    t.text "caption", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -26,4 +38,5 @@ ActiveRecord::Schema.define(version: 2020_09_22_074839) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tweets", "users"
 end
