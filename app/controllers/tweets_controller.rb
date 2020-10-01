8ab6c32c9f2b7b728c91b145f875a0e1dfee8f3c
tatsu0713 <kwbttk@icloud.com>
2020-10-01 12:43:11 +0900

    gem rails_12factorの追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,15 +4,22 @@ class TweetsController < ApplicationController
   end
 
   def new
+    @tweet = Tweet.new
   end
   
   def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
 
   private
 
-  # def tweet_params
-  #   params.require(:tweet).permit(:image, :brand_id, :type_id, :model_year_id, :title, :caption).merge(user_id: current_user.id)
-  # end
+  def tweet_params
+    params.permit(:image, :brand_id, :type_id, :model_year_id, :title, :caption).merge(user_id: current_user.id)
+  end
 end
